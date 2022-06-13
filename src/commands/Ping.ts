2752fa0b6f6f589ad8f6524a38a690d6b7ebe5e0
Gurkan <hello@gurkz.me>
2022-06-13 15:38:03 +0200

    Modified Ping command to include the ms

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -7,25 +7,8 @@ export default {
 	slash: 'both', // Create both a slash and legacy command
 	testOnly: true, // Only register a slash command for the testing guilds
 
-	callback: ({ message, interaction }) => {
-		const reply = 'Pong!';
-
-		// message is provided for a legacy command
-		if (message) {
-			message.reply({
-				content: reply,
-			});
-			return;
-		}
-
-		// interaction is provided for slash commands
-		interaction.reply({
-			content: reply,
-		});
-
-		// Alternatively we can just simply return our reply object
-		// OR just a string as the content.
-		// WOKCommands will handle the proper way to reply with it
+	callback: ({ client }) => {
+		const reply = `🏓 Pong! My ping is ${client.ws.ping}ms.`;
 		return {
 			content: reply,
 		};
